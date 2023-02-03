ffaa618370f1861058e32742f60a463463a016d8
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-02-03 13:36:52 +0000

    Add styles and router to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="45" />
@@ -12,13 +13,17 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <Nav.Link 
+            exact 
+            className={styles.NavLink} 
+            activeClass={styles.Active} 
+            to="/">
               <i className="fas fa-home"></i>Home
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link className={styles.NavLink} activeClass={styles.Active} to="/signin">
               <i className="fas fa-sign-in-alt"></i>Sign in
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link className={styles.NavLink} activeClass={styles.Active}  to="/signup">
               <i className="fas fa-user-plus"></i>Sign up
             </Nav.Link>
           </Nav>
